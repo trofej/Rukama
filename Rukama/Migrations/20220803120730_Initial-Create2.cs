478a4ecaf2f13c226d90d268cdeb0f2b67a42c1e
trofej <timon.fopp@uzh.ch>
2022-08-05 10:47:40 +0200

    Add project files.

---
+++ Rukama/Migrations/20220803120730_Initial-Create2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rukama.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "surname",
+                table: "AspNetUsers",
+                newName: "Surname");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "AspNetUsers",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Surname",
+                table: "AspNetUsers",
+                newName: "surname");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "AspNetUsers",
+                newName: "name");
+        }
+    }
+}
---
+++ Rukama/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rukama.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
